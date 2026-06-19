9ce9652783920fb4506353afc1c3d582da64d868
Thaylisson <silva.thaylisson@escola.pr.gov.br>
2026-06-19 08:28:57 -0300

    Create Clientes table with initial data

---
+++ banco_1906.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Clientes (
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(50),
+    idade INT,
+    cidade VARCHAR(50),
+    status VARCHAR(20)
+);
+
+-- Inserção de dados na tabela
+INSERT INTO Clientes VALUES
+(1, 'Ana', 25, 'Curitiba', 'Ativo'),
+(2, 'Bruno', 32, 'Londrina', 'Ativo'),
+(3, 'Carla', 29, 'Maringá', 'Inativo'),
+(4, 'Diego', 40, 'Curitiba', 'Ativo'),
+(5, 'Elisa', 22, 'Maringá', 'Ativo');
