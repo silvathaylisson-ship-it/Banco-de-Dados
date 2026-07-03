713ac1ddd802d0d381f50d1666f937aa29cc7fb3
Thaylisson <silva.thaylisson@escola.pr.gov.br>
2026-07-03 08:35:55 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -1,13 +1,17 @@
+SELECT * FROM Clientes;
+
 CREATE TABLE Clientes (
     ID INTEGER PRIMARY KEY,
     nomeCliente VARCHAR(255),
     emailCliente VARCHAR(255)
 );
 
--- Criação da Tabela Compras
 CREATE TABLE Compras (
     CompraID INTEGER PRIMARY KEY,
     ClienteID INTEGER,
     NomeLivro VARCHAR(255),
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+SELECT * FROM Clientes;
+SELECT * FROM Compras;
